60a477584e18c9d9f7576558a40eb9acee820358
RajatSablok <rajat.main06@gmail.com>
2020-04-19 21:45:43 +0530

    converted to api

--- app.js
+++ app.js
@@ -1,70 +1,25 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const fs = require('fs')
-const json2csv = require('json2csv').Parser
+const express = require('express');
+const app = express();
 
-var titleArray = []
-var contentArray = []
-var result = {};
-var inshortsData = {}
-var title
-var content
-var titleObj = []
-var resultObj = []
+const allNews = require('./api/routes/english/all');
 
+app.use("/en/all", allNews);
 
-request('https://inshorts.com/en/read/', (error, response, html) => {
-    if( !error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-      
-        $('.card-stack').each((i,el) => {
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+  
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+});
 
-            title = $(el)
-            .find('.news-card-title a')
-            .find('span').text()
+const PORT = process.env.PORT || 3000;
 
-            titleArray = title.split('short')
-
-            // for (var i = 0; i < titleArray.length; i++) {
-            //     titleObj.push({"title": titleArray[i]})
-            // }
-            
-            content = $(el)
-            .find('.news-card-content')
-            .find('div')
-            .text()
-            .trim()
-            .replace(/short by .+\s\s+.+/gm, 'ezpz')
-            .trim()
-            
-            contentArray = content.split('ezpz')
-
-            for (var i = 0; i < contentArray.length; i++) {
-                contentArray[i] = contentArray[i].trim()
-            }
-
-            link = $(el)
-            .find('.news-card-author-time a ')
-
-            for (var i = 0; i < titleArray.length; i++) {
-
-                resultObj.push({
-                    
-                    "title": titleArray[i],
-                    "news": contentArray[i]
-                  })
-            }
-
-        })
-
-        inshortsData = {"data" : resultObj}
-
-        console.log(inshortsData)
-
-        const j2cp = new json2csv()
-        const csv = j2cp.parse(inshortsData.data)
-        
-        fs.writeFileSync("./inshorts.csv", csv, "utf-8")
-
-    }
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
