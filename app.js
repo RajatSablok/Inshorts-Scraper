88bb056cd1d9b485c454c8aead385d3a6c50cc76
RajatSablok <rajat.main06@gmail.com>
2020-04-19 20:24:59 +0530

    better i/p management

--- app.js
+++ app.js
@@ -5,43 +5,87 @@ const writeStream = fs.createWriteStream('news.csv');
 
 // Write Headers
 writeStream.write(`Title,Content \n`);
+var titleArray = []
+var contentArray = []
+var result = {};
+const inshortsData = []
+var title
+var content
+var titleObj = []
+var resultObj = []
+
 
 request('https://inshorts.com/en/read/', (error, response, html) => {
     if( !error && response.statusCode == 200) {
         const $ = cheerio.load(html);
-
+      
         $('.card-stack').each((i,el) => {
 
-            const title = $(el)
+            title = $(el)
             .find('.news-card-title a')
             .find('span').text()
-            .replace(/short+/g, '\n');
 
-            titleArray = title.split('\n')
+            titleArray = title.split('short')
 
-            const content = $(el)
+            for (var i = 0; i < titleArray.length; i++) {
+                titleObj.push({"title": titleArray[i]})
+            }
+            
+            content = $(el)
             .find('.news-card-content')
             .find('div')
-            .text() 
-            .replace(/short by .+\s\s+.+/gm, '')
+            .text()
+            .trim()
+            .replace(/short by .+\s\s+.+/gm, 'ezpz')
+            .trim()
+            
+            contentArray = content.split('ezpz')
 
-            contentt = content.replace(/\s\s+/gm, 'mynewhack')
+            for (var i = 0; i < contentArray.length; i++) {
+                contentArray[i] = contentArray[i].trim()
+            }
 
-            contentArray = contentt.split('mynewhack')
-
-            var result = {};
-            titleArray.forEach((key, i) => result[key] = contentArray[i]);
-
-            console.log(title)
-            console.log(titleArray)
-            console.log(content)
             console.log(contentArray)
 
-            console.log(result);
+            //follow next two lines to remove the date and time (not needed in this version)
+            // contentt = content.replace(/\s\s+/gm, 'mynewhack')
+            // contentArray = contentt.split('mynewhack')
+
+            titleArray.forEach((key, i) => result[key] = contentArray[i]);
+
+            link = $(el)
+            .find('.news-card-author-time a ')
+
+            for (var i = 0; i < titleArray.length; i++) {
+
+                // resultObj.push({"title": titleArray[i]}, {"news": contentArray[i]})
+
+                resultObj.push({
+                    
+                    "title": titleArray[i],
+                    "news": contentArray[i]
+                  })
+
+                
+            }
+
+            // console.log(titleArray)
+            // console.log(content)
+            // console.log(contentArray)
+
+            // console.log(result);
 
         // Write Row To CSV
         // writeStream.write(`${title}, ${newwContent} \n`);
 
         })
+        console.log(titleArray)
+        console.log('test')
+        console.log(contentArray[5])
+        console.log('test')
+        // console.log(result)
+        console.log(resultObj)
+
+
     }
 })
